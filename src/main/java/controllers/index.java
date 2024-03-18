f620d862d2176e054c5e0274f27dc1d85059f77f
Elisa Bothy <elisa.bothy@gmail.com>
2024-03-18 21:50:33 +0100

    Structure du projet Création des différents folders, du template de départ, mise en place de l'index pour qu'il s'ouvre bien sur la page /visitor/index Pour que plus tard les photos et autres sources et le même chemin et qu'on ne soit pas bloqués. ajout de la dépndency jstl dans le pom pour pouvoir utiliser <c: En résumé, mise en place succinte du projet

--- src/main/java/controllers/index.java
+++ src/main/java/controllers/index.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Herbert Caffarel
  */
-@WebServlet("/index")
+@WebServlet("/visitor/index")
 @SuppressWarnings("serial")
 public class index extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/visitor/index.jsp").forward(req, resp);
     }
 
 }
---
+++ src/main/webapp/WEB-INF/visitor/index.jsp
@@ -0,0 +1,12 @@
+<%-- 
+    Document   : index
+    Created on : 18 mars 2024, 15:51:41
+    Author     : Elisa Bothy
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:include page="/WEB-INF/template/template.jsp">
+<jsp:param name="title" value="Accueil"/>
+<jsp:param name="content" value="/WEB-INF/contents/index"/>
+</jsp:include>
+
