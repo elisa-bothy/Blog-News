9bc70483963840ec5462bdd8928146011cd8b517
Guigui <guirosta@gmail.com>
2024-03-19 09:38:44 +0100

    Création des classes d'entités Person, News, Comment et Vote

---
+++ src/main/java/entities/Person.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Guillaume Rostagnat
+ */
+public class Person {
+    
+    Integer id;
+    String Login;
+    String Password;
+    
+    
+    
+}
